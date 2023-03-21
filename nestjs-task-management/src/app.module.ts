fbf23aa775d0a8dc5d76074530ef62c11e308711
Bosshentai <baptistamhernani@gmail.com>
2023-03-21 07:43:49 -0100

    Learning NestJS

---
+++ nestjs-task-management/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TasksModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'learning',
+      password: 'learning',
+      database: 'learning',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+
+  // controllers: [],
+  // providers: [],
+})
+export class AppModule {}
---
+++ nestjs-task-management/src/tasks/tasks.controller.ts
@@ -0,0 +1,71 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-task-filter.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-filter.dto';
+
+@Controller('tasks')
+export class TasksController {
+  // tasksService: TasksService;
+
+  constructor(private tasksService: TasksService) {
+    // this.tasksService = tasksService;
+  }
+
+  @Get()
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      return this.tasksService.getAllTasks();
+    }
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    const found = this.tasksService.getTaskById(id);
+
+    if (!found) {
+      throw new NotFoundException(`Task with Id ${id} not found`);
+    }
+
+    return found;
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    // console.log('title', title);
+    // console.log('description', description);
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  // @Post()
+  // createTask(@Body() body) {
+  //   console.log(body);
+  // }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ): Task {
+    const { status } = updateTaskStatusDto;
+    return this.tasksService.updateTaskStatus(id, status);
+  }
+}
